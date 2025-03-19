270b5c4371d2227d040170a1d3313e8b5d0310f0
Nicolas Guilland <117724617+Nic0ooo@users.noreply.github.com>
2025-03-19 12:32:24 +0100

    feat/ user class

--- src/main/java/fr/esgi/color_run/business/User.java
+++ src/main/java/fr/esgi/color_run/business/User.java
@@ -1,4 +1,35 @@
 package fr.esgi.color_run.business;
 
+import java.util.List;
+import lombok.Data;
+
+/**
+ * Représente un utilisateur sur l'app
+ */
+@Data
 public class User {
+    private Long id;
+    private Role role;
+    private String name;
+    private String firstname;
+    private String email;
+    private String password;
+    private Integer phoneNumber;
+    private String address;
+    private String city;
+    private Integer zipCode;
+    private double positionLatitude;
+    private double positionLongitude;
+
+    private static Long compteur = 0L;
+    /**
+     * Constructeur par défaut de la classe User.
+     * Initialise l'identifiant, le role à coureur et les coordonnées à celle de Paris.
+     */
+    public User() {
+        this.id = compteur++;
+        this.role = Role.RUNNER;
+        this.positionLatitude = 48.8566;
+        this.positionLongitude = 2.3522;
+    }
 }
