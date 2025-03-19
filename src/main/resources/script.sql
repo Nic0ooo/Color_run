1c76c609d849188de2578cab90253e9db0fdcc54
Nicolas Guilland <117724617+Nic0ooo@users.noreply.github.com>
2025-03-19 14:29:10 +0100

    feat/ script sql

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -13,3 +13,16 @@ CREATE TABLE User (
                       positionLatitude DOUBLE,
                       positionLongitude DOUBLE
 );
+
+CREATE TABLE Association (
+                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                    name VARCHAR(255),
+                    description VARCHAR(255),
+                    websiteLink VARCHAR(255),
+                    logoPath VARCHAR(255),
+                    email VARCHAR(255),
+                    phoneNumber INTEGER,
+                    address VARCHAR(255),
+                    city VARCHAR(255),
+                    zipCode INTEGER,
+);
